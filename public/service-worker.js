b13a73458481271627a9f876f21e90128b1952cc
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-11-10 08:12:42 -0600

    added caches code to service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,53 @@
+const { response } = require("express");
+
+var CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "db/js",
+    "/manifest.json",
+    "styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+];
+
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(event.request);
+                });
+            }).catch(err => console.log(err))
+        );
+        
+        return;
+    }
+
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+          return cache.match(evt.request).then(response => {
+            return response || fetch(evt.request);
+          });
+        })
+      );
+})
